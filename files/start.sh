8d65eea92bf5ba07a34c735da30c4decb8269293
xusslavv <147701976+xusslavv@users.noreply.github.com>
2023-10-17 16:38:06 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #定义哪吒变量参数
-export NEZHA_SERVER="nz.f4i.cn:5555"
-export NEZHA_KEY="5ddVS93Eq0Uc9he880"
+export NEZHA_SERVER="nezha.sslav.eu.org:5555"
+export NEZHA_KEY="QPF50E1pKPh3wLOms0"
 
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
